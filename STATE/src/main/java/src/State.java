b3fa39967c4a1560bbdc40c3755294d4d2737db8
yhojana <yhojanauscamayta123@gmail.com>
2022-11-25 03:21:04 -0500

    State

---
+++ STATE/src/main/java/src/State.java
@@ -0,0 +1,12 @@
+package src;
+
+public interface State {
+
+    
+    void jugar();
+    void alimentar();
+    void dormir();
+    void comoEstas();
+
+    void setTamagotchi(Tamagotchi tamagotchi);
+}
